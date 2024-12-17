88d5a74b7226bb9c77efa368441635ccf598cd05
Kent Ball <kent.vincent.ball@gmail.com>
2024-12-17 12:56:00 -0500

    Changed home page layout, header, footer and movie cards to a black background

--- client/src/components/Layout/Footer/Footer.jsx
+++ client/src/components/Layout/Footer/Footer.jsx
@@ -5,10 +5,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-          This product uses the TMDB API but is not endorsed or certified by TMDB.
-        </p>
-        <p>
-          Movie Ratings App was designed in 2024 by Kent Ball.
+          This product uses the TMDB API but is not endorsed or certified by TMDB. Movie Ratings App was designed in 2024 by Kent Ball.
           <a 
             href="https://spectacular-kitsune-fb2d4e.netlify.app" 
             target="_blank" 
