2ea8ec856ef382e5b5785bf3ed165510c2144065
Kent Ball <kent.vincent.ball@gmail.com>
2025-01-18 12:54:48 -0500

    Updated apolloClient setup to include deployed site address

--- client/src/utils/apolloClient.js
+++ client/src/utils/apolloClient.js
@@ -2,7 +2,9 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: process.env.NODE_ENV === 'production' 
+    ? 'https://movie-ratings-react.onrender.com/graphql' 
+    : 'http://localhost:3001/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
